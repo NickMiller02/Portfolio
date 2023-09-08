24d57153b3dfed339223bc58919ac355e6e60760
NickMiller02 <nicholasmill157@gmail.com>
2023-09-08 15:36:15 -0400

    changing to chakra

--- src/App.js
+++ src/App.js
@@ -7,18 +7,26 @@ import Home from './components/Home';
 import Resume from './components/Resume';
 import Portfolio from './components/Portfolio';
 
+import '../src/index.css'
+import { Flex } from '@chakra-ui/react';
+import { Box } from '@mui/material';
+
 // Rendering App Page
 export const App = () => {
   
   return (
     <Router>
+      <Flex flexDirection="column" minHeight="100vh"> 
       <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+        <Box flexGrow={1}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Box>
+      </Flex>
     </Router>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import theme from '..';
 import { Link } from 'react-router-dom';
-import { Typography, AppBar, CssBaseline, Toolbar, Stack, Button } from '@mui/material';
+import { TabList, TabPanels, TabPanel, Tab, Flex } from '@chakra-ui/react';
 
 
 // Nav Function
@@ -10,33 +10,12 @@ export const Nav = () => {
 
 
     return (
-        <>  
-            {/* Styles Nav Bar */}
-            <CssBaseline />
-            <AppBar sx={{ display: "flex", bgcolor: theme.palette.secondary.main }}>
-                <Toolbar>
-                    <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-                        Nick's Portfolio
-                    </Typography>
-
-                    {/* Lists and Links each page/tab */}
-                    <Stack direction='row' spacing={2}>
-                        <Link to='/'>
-                            <Button variant='contained'>Home</Button>
-                        </Link>
-                        <Link to='portfolio'>
-                            <Button variant='contained'>Portfolio</Button>
-                        </Link>
-                        <Link to='resume'>
-                            <Button variant='contained'>Resume</Button>
-                        </Link>
-                        <Link to='contact'>
-                            <Button variant='contained'>Contact</Button>
-                        </Link>
-                    </Stack>
-                </Toolbar>
-            </AppBar>
-        </>
+        <Flex alignItems="center">
+            <a href='/'>Home</a>
+            <a href='/portfolio'>Portfolio</a>
+            <a href='/resume'>Resume</a>
+            <a href='/contact'>Contact</a>
+        </Flex>
     )
 }
 
