0cb15ba5b89b0f840df3888cc45995c6a21e099f
NickMiller02 <nicholasmill157@gmail.com>
2023-10-06 13:21:31 -0400

    working on home components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 // Imports - React & Chakra UI
 import React from 'react';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
-import { Box, Flex, Stack, useMediaQuery } from '@chakra-ui/react';
+import { Box, Flex, HStack, Stack, Text, useMediaQuery } from '@chakra-ui/react';
 
 // Home Function
 export const Home = () => {
@@ -9,14 +9,21 @@ export const Home = () => {
     const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
 
     return (
-        <Stack>
+        <Stack spacing={1} alignItems='center'>
             <Flex direction={isNotSmallerScreen ? "row" : "column"} 
             spacing="200px" p={isNotSmallerScreen ? '32' : "0"} 
             alignSelf="flex-start">
                 <Box mt={isNotSmallerScreen ? '0' : 16} align="flex-start">
-                    
+                    <Text fontSize='5x1' fontWeight="semibold"> Hello, I am </Text>
+                    <Text fontSize='50px' fontWeight="bold"> Nick Miller </Text>
                 </Box>
             </Flex>
+            <Box p="5" mt={isNotSmallerScreen ? '0' : 16} align="center" border="2px" boxShadow="xl" rounded="md" w='1000px'>
+                <Text fontSize="25px" fontWeight="bold">Check Out My Most Recent Project!</Text>
+                <HStack>
+                    
+                </HStack>
+            </Box>
         </Stack>
     )
 }
