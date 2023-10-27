f86b0b693ed2fcafcdf380c59312702f16e239ba
NickMiller02 <nicholasmill157@gmail.com>
2023-10-27 14:17:04 -0400

    home page structure outline

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 // Imports - React & Chakra UI
 import React from 'react';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
-import { Box, Container, Flex, HStack, Stack, Text, useMediaQuery } from '@chakra-ui/react';
+import { Box, Circle, Container, Flex, HStack, Image, Stack, Text, useColorMode, useMediaQuery } from '@chakra-ui/react';
 
 // Home Function
 export const Home = () => {
     
+    // Dark mode using Color mode
+    const { colorMode } = useColorMode();
+    const isDark = colorMode === "dark";
+
+    // Screen Sizing
     const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
 
     return (
@@ -20,7 +25,7 @@ export const Home = () => {
             </Flex>
             <Box bgColor="#2b6cb0" minW="100%">
                 <HStack>
-                    <Box p="1" m={isNotSmallerScreen ? '16' : 32} align="center" border="2px" boxShadow="xl" rounded="md">
+                    <Box p="1" m={isNotSmallerScreen ? '16' : 32} bg='red.100' align="center" border="2px" boxShadow="xl" rounded="md">
                         <Text fontSize="25px" fontWeight="bold">Check Out My Most Recent Project!</Text>
                     </Box>
                     <Box p="1" m={isNotSmallerScreen ? '16' : 32} align="center" border="2px" boxShadow="xl" rounded="md">
@@ -28,6 +33,34 @@ export const Home = () => {
                     </Box>
                     <Box p="1" m={isNotSmallerScreen ? '16' : 32} align="center" border="2px" boxShadow="xl" rounded="md">
                         <Text fontSize="25px" fontWeight="bold">Check Out My Most Recent Project!</Text>
+                    </Box>
+                </HStack>
+            </Box>
+            <Stack>
+                <Circle position="absolute" bg="blue.100" opacity="0.3"
+                    w="300px" h="300px" alignSelf="flex-start" />
+                <Flex direction={isNotSmallerScreen ? "row" : "column"} 
+                spacing="200px" p={isNotSmallerScreen ? '32' : "0"} 
+                alignSelf="flex-start">
+                <Image alignSelf="center" mt={isNotSmallerScreen ? '0' : 12}
+                    mb={isNotSmallerScreen ? "0" : "12"} borderRadius="full"
+                    backgroundColor="transparent" boxShadow="lg"
+                    boxSize="300px" src='https://avatars.githubusercontent.com/u/113201258?v=4' />
+                <Box p='1' m={isNotSmallerScreen ? '16' : 32} align="flex-end" bgColor='red.100'>
+                    <Text m='2' fontSize='50px' fontWeight="bold" align="center">About Me</Text>
+                        <Text fontSize="22px" fontWeight="semibold">Check Out My Most Recent Project!</Text>
+                </Box>
+                </Flex>
+            </Stack>
+            <Box mt={isNotSmallerScreen ? '0' : 16} align="flex-start">
+                <HStack>
+                    <Box m={isNotSmallerScreen ? '0' : 16} align="flex-start">
+                        <Text fontSize='60px' fontWeight='bold'>Contact Me</Text>
+                    </Box>
+                    <Box m={isNotSmallerScreen ? '0' : 16}>
+                        <Box p="1" m={isNotSmallerScreen ? '16' : 32} align="center" border="2px" boxShadow="xl" rounded="md">
+                            <Text fontSize="25px" fontWeight="bold">Check Out My Most Recent Project!</Text>
+                        </Box>
                     </Box>
                 </HStack>
             </Box>
