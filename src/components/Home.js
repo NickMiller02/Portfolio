3666fb0dcaa0f83c5fd728a3267377415360eec8
NickMiller02 <nicholasmill157@gmail.com>
2023-11-03 13:51:35 -0400

    project section/contact/about in progress, dark mode palette working

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,47 +23,59 @@ export const Home = () => {
                     <Text fontSize='50px' fontWeight="bold"> Nick Miller </Text>
                 </Box>
             </Flex>
-            <Box bgColor="#2b6cb0" minW="100%">
-                <HStack>
-                    <Box p="1" m={isNotSmallerScreen ? '16' : 32} bg='red.100' align="center" border="2px" boxShadow="xl" rounded="md">
-                        <Text fontSize="25px" fontWeight="bold">Check Out My Most Recent Project!</Text>
-                    </Box>
-                    <Box p="1" m={isNotSmallerScreen ? '16' : 32} align="center" border="2px" boxShadow="xl" rounded="md">
-                        <Text fontSize="25px" fontWeight="bold">Check Out My Most Recent Project!</Text>
-                    </Box>
-                    <Box p="1" m={isNotSmallerScreen ? '16' : 32} align="center" border="2px" boxShadow="xl" rounded="md">
-                        <Text fontSize="25px" fontWeight="bold">Check Out My Most Recent Project!</Text>
-                    </Box>
+            <Box bgColor={isDark ? 'gray.700' : "#2b6cb0"} minW="100%" alignItems='center'>
+                <HStack spacing={1} alignItems='center'>
+                    <Flex direction={isNotSmallerScreen ? "row" : "column"} 
+                        spacing="200px" p={isNotSmallerScreen ? '32' : "0"} 
+                        alignSelf="flex-start">
+                        <Box p="1" m={isNotSmallerScreen ? '16' : 32} bgColor={isDark ? 'gray.600' : 'gray.100'} alignSelf="flex-start" border="2px" boxShadow="xl" rounded="md">
+                            <Stack>
+                                <Box m='1.5' w='200px' h='200px' bgColor='red.100' alignSelf='center' />
+                                <Text fontSize='25px' fontWeight="semibold">Title of Project</Text>
+                            </Stack>
+                        </Box>
+                        <Box p="1" m={isNotSmallerScreen ? '16' : 32} bgColor={isDark ? 'gray.600' : 'gray.100'} alignSelf="flex-start" border="2px" boxShadow="xl" rounded="md">
+                            <Stack>
+                                <Box m='1.5' w='200px' h='200px' bgColor='red.100' alignSelf='center' />
+                                <Text fontSize='25px' fontWeight="semibold">Title of Project</Text>
+                            </Stack>
+                        </Box>
+                        <Box p="1" m={isNotSmallerScreen ? '16' : 32} bgColor={isDark ? 'gray.600' : 'gray.100'} alignSelf="flex-start" border="2px" boxShadow="xl" rounded="md">
+                            <Stack>
+                                <Box m='1.5' w='200px' h='200px' bgColor='red.100' alignSelf='center' />
+                                <Text fontSize='25px' fontWeight="semibold">Title of Project</Text>
+                            </Stack>
+                        </Box>
+                    </Flex>
                 </HStack>
             </Box>
-            <Stack>
-                <Circle position="absolute" bg="blue.100" opacity="0.3"
-                    w="300px" h="300px" alignSelf="flex-start" />
-                <Flex direction={isNotSmallerScreen ? "row" : "column"} 
-                spacing="200px" p={isNotSmallerScreen ? '32' : "0"} 
-                alignSelf="flex-start">
-                <Image alignSelf="center" mt={isNotSmallerScreen ? '0' : 12}
-                    mb={isNotSmallerScreen ? "0" : "12"} borderRadius="full"
-                    backgroundColor="transparent" boxShadow="lg"
-                    boxSize="300px" src='https://avatars.githubusercontent.com/u/113201258?v=4' />
+            <HStack>
+                <Stack>
+
+                    <Circle position="absolute" bg="blue.100" opacity="0.3"
+                        w="300px" h="300px" alignSelf="flex-start" />
+                    <Flex direction={isNotSmallerScreen ? "row" : "column"} 
+                    spacing="200px" p={isNotSmallerScreen ? '32' : "16"} 
+                    alignSelf="flex-start">
+                    <Image alignSelf="center" mt={isNotSmallerScreen ? '0' : 12}
+                        mb={isNotSmallerScreen ? "0" : "12"} borderRadius="full"
+                        backgroundColor="transparent" boxShadow="lg"
+                        boxSize="300px" src='https://avatars.githubusercontent.com/u/113201258?v=4' />
+                    </Flex>
+
+                    <Box p='1' m={isNotSmallerScreen ? '16' : 32} align="flex-end" bgColor={isDark ? 'gray.700' : 'gray.100'} border='2px' boxShadow='xl' rounded='md'>
+                        <Text m='2' fontSize='50px' fontWeight="bold" align="center">Contact Me</Text>
+                        <Text fontSize="22px" fontWeight="semibold">Check Out My Most Recent Project!</Text>
+                    </Box>
+                </Stack>
+
                 <Box p='1' m={isNotSmallerScreen ? '16' : 32} align="flex-end" bgColor='red.100'>
                     <Text m='2' fontSize='50px' fontWeight="bold" align="center">About Me</Text>
-                        <Text fontSize="22px" fontWeight="semibold">Check Out My Most Recent Project!</Text>
+                    <Text fontSize="22px" fontWeight="semibold">Check Out My Most Recent Project!</Text>
                 </Box>
-                </Flex>
-            </Stack>
-            <Box mt={isNotSmallerScreen ? '0' : 16} align="flex-start">
-                <HStack>
-                    <Box m={isNotSmallerScreen ? '0' : 16} align="flex-start">
-                        <Text fontSize='60px' fontWeight='bold'>Contact Me</Text>
-                    </Box>
-                    <Box m={isNotSmallerScreen ? '0' : 16}>
-                        <Box p="1" m={isNotSmallerScreen ? '16' : 32} align="center" border="2px" boxShadow="xl" rounded="md">
-                            <Text fontSize="25px" fontWeight="bold">Check Out My Most Recent Project!</Text>
-                        </Box>
-                    </Box>
-                </HStack>
-            </Box>
+
+            </HStack>
+            
         </Stack>
     )
 }
